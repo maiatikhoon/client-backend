b3a4eab1cef9820f4b59c0ad2936e9d7dc72d45e
maiatikhoon <alam.atik081@gmail.com>
2024-03-01 11:22:26 +0530

    added joi validation

--- controller/userController.js
+++ controller/userController.js
@@ -2,10 +2,21 @@ const User = require("../model/userModel");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const { v4: uuidv4 } = require("uuid");
+const Joi = require("joi");
 
 const registerUser = async (req, res) => {
   try {
     const { username, email, password, cnf_password } = req.body;
+
+    const data = { username, email, password };
+
+    const { error } = validateRegisterInput(data);
+
+    if (error) {
+      let errorMessage = error.details[0].message;
+      errorMessage = errorMessage.replace(/"/g, "");
+      return res.status(400).json({ error: errorMessage });
+    }
 
     if (password !== cnf_password) {
       return res.status(500).json({ error: "passwords don't matched" });
@@ -48,6 +59,16 @@ const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
 
+    const { error } = validateLoginInput(req.body);
+
+    if (error) {
+      let errorMessage = error.details[0].message;
+
+      errorMessage = errorMessage.replace(/"/g, "");
+
+      return res.status(400).json({ error: errorMessage });
+    }
+
     const user = await User.findOne({ email: email });
 
     if (user) {
@@ -73,6 +94,46 @@ const loginUser = async (req, res) => {
     console.log(error);
     res.status(500).json({ eror: "Internal server error" });
   }
+};
+
+const validateRegisterInput = (data) => {
+  const schema = Joi.object({
+    username: Joi.string().required().min(3).max(30).messages({
+      "string.min": "username should be atleast 3 character long",
+      "string.max": "maximum 30 characters only",
+    }),
+    email: Joi.string()
+      .email({ tlds: { allow: ["com", "in", "net", "ac"] } })
+      .required()
+      .messages({
+        "string.email": "enter a valid email address ",
+      })
+      .required(),
+    password: Joi.string().min(3).required().messages({
+      "string.min": "password should be atleast 3 character long",
+    }),
+  });
+
+  return schema.validate(data);
+};
+
+const validateLoginInput = (data) => {
+  const schema = Joi.object({
+    email: Joi.string()
+      .required()
+      .email({
+        tlds: { allow: ["com", "in", "net", "ac"] },
+      })
+      .messages({
+        "string.email": "enter a valid email address",
+      }),
+
+    password: Joi.string().min(3).required().messages({
+      "string.min": "password should be ateast 3 characters",
+    }),
+  });
+
+  return schema.validate(data);
 };
 
 module.exports = { registerUser, loginUser };
